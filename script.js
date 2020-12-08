d79a9790d2d3d087f477e88e0306778b3a15640d
andurebel <andu.trica@gmail.com>
2020-12-08 22:09:52 +0200

    added local storage

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 // Add a transaction
 
@@ -31,7 +36,10 @@ function addTransaction(e) {
     transactions.push(transaction);
 
     addTransactionDOM(transaction);
+
     updateValues();
+
+    updateLocalStorage();
 
     text.value = "";
     amount.value = "";
@@ -91,7 +99,15 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
 
+  updateLocalStorage();
+
   init();
+}
+
+//Update local storage transactions
+
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
 //Initialize app
@@ -100,6 +116,7 @@ function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
   updateValues();
+  updateLocalStorage();
 }
 
 init();
